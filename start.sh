54e37a10e445fe0036f438681a2161f59bba760a
Johannes Raggam <raggam-nl@adm.at>
2012-02-23 10:32:03 +0100

    startstopskripts

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+./bin/zeoserver start
+./bin/instance start
+./bin/instance2 start
+./bin/instance3 start
+./bin/instance4 start
+./bin/instance5 start
+./bin/instance6 start
---
+++ stop.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+./bin/instance stop
+./bin/instance2 stop
+./bin/instance3 stop
+./bin/instance4 stop
+./bin/instance5 stop
+./bin/instance6 stop
+./bin/zeoserver stop
